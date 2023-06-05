3a1aa2aa802162bf2f8ba6211afc395e627356db
Quadri Odunuga <quadriodunuga@Quadris-MacBook-Pro.local>
2023-06-05 02:56:32 +0100

    I edited the web page

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ window.onload = function () {
 //     //}
 // }
 
-const passwordInput = document.getElementById('password-input');
-const togglePassword = document.getElementById('toggle-password');
+// const passwordInput = document.getElementById('password-input');
+// const togglePassword = document.getElementById('toggle-password');
 
-togglePassword.addEventListener('click', function () {
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        togglePassword.classList.remove('fa-eye');
-        togglePassword.classList.add('fa-eye-slash');
-    } else {
-        passwordInput.type = 'password';
-        togglePassword.classList.remove('fa-eye-slash');
-        togglePassword.classList.add('fa-eye');
-    }
-});
+// togglePassword.addEventListener('click', function () {
+//     if (passwordInput.type === 'password') {
+//         passwordInput.type = 'text';
+//         togglePassword.classList.remove('fa-eye');
+//         togglePassword.classList.add('fa-eye-slash');
+//     } else {
+//         passwordInput.type = 'password';
+//         togglePassword.classList.remove('fa-eye-slash');
+//         togglePassword.classList.add('fa-eye');
+//     }
+// });
